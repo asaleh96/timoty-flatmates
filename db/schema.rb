4c327b499cee74e0d40017a369dbd4d013eb071a
caroline bras <brascaroline64@gmail.com>
2022-11-17 20:32:17 +0100

    New model for Rules and updated users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_174200) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_192205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rules", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "household_id"
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +29,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_174200) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.integer "earned_points"
+    t.integer "give_points"
+    t.date "birthday_date"
+    t.text "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

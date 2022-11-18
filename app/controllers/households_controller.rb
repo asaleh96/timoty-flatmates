23c13bd6468e7106af2e239490b56439e316c6d8
Shahid Farooq <sif101@hotmail.co.uk>
2022-11-18 14:59:34 +0000

    added controllers and a few views

---
+++ app/controllers/households_controller.rb
@@ -0,0 +1,52 @@
+class HouseholdsController < ApplicationController
+
+  def index
+    @households = Household.all
+  end
+
+  def show
+    @household = Household.find(params[:id])
+  end
+
+  def new
+    @household = Household.new
+  end
+
+  def create
+    params = household_params
+    params[:captain_id] = current_user.id
+    @household = Household.new(params)
+    @household.save!
+    if @household.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @household = Household.find(params[:id])
+  end
+
+  def update
+    @household = Household.find(params[:id])
+    @household.update(household_params)
+    redirect_to household_path(@household)
+  end
+
+  def destroy
+    @household = Household.find(params[:id])
+    @household.destroy
+    redirect_to households_path
+  end
+
+  def users
+    @users = User.all
+  end
+
+  private
+
+  def household_params
+    params.require(:household).permit(:name, :captain_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :households, only: %i[index show new create edit update destroy] do
+    resources :tasks, only: %i[index show new create edit update destroy]
+  end
+  resources :users, only: %i[index show new create edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

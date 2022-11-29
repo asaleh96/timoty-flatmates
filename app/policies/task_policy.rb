40dd93ae74e7c16415ee6f7f0a877a71946c7d5f
caroline bras <brascaroline64@gmail.com>
2022-11-29 21:14:09 +0100

    pundit cleanup
    Signed-off-by: caroline bras <brascaroline64@gmail.com>

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,28 +1,2 @@
 class TaskPolicy < ApplicationPolicy
-  # class Scope < Scope
-  #   NOTE: Be explicit about which records you allow access to!
-  #   def resolve
-  #     scope.all
-  #   end
-  # end
-
-  def new?
-    true
-  end
-
-  def create?
-    new?
-  end
-
-  def edit?
-    true
-  end
-
-  def update?
-    true
-  end
-
-  def destroy?
-    true
-  end
 end

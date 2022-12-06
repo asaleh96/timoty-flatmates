9bbaa61ee7eb50ec7533f7f73787cd7307f1f37b
caroline bras <brascaroline64@gmail.com>
2022-12-06 20:34:27 +0100

    controller for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,25 @@
 class ActivitiesController < ApplicationController
+  before_action :set_household, only: %i[index new create edit update destroy]
+
+  def new
+    @household = Household.find(params[:household_id])
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activiy.new(activity_params)
+    @activity.household = @household
+    @activity.save
+    redirect_to household_path(@household)
+  end
+
+  private
+
+  def set_household
+    @household = Household.find(params[:household_id])
+  end
+
+  def review_params
+    params.require(:activity).permit(:content)
+  end
 end

4a67c768102c5da72fe87ca7dabc3dc7d81db774
Shahid Farooq <105807541+SIFAR101@users.noreply.github.com>
2022-12-06 10:40:02 +0000

    defined sidekiq job, need to connect to a rake task

--- app/jobs/reset_game_job.rb
+++ app/jobs/reset_game_job.rb
@@ -1,8 +1,15 @@
 class ResetGameJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform
     # Do something later
-    puts "Resetting game"
+    Household.all.each do |household|
+      User.where(household_id: household.id).each do |user|
+        user.update_attributes(points: 0)
+      end
+      Task.where(household_id: household.id).each do |task|
+        task.destroy
+      end
+    end
   end
 end

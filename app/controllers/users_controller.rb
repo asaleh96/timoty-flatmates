5858956f8b6845a58d5fced1dcad229884b9ce99
Simona Barone <simona_barone@hotmail.com>
2022-12-10 11:09:00 +0100

    fixed cloudinary, added about page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :birthday_date, :photo, :status, :earned_points, :photo)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :birthday_date, :photo, :status, :earned_points)
   end
 
   def set_user
